cedd4e9d207c1e82d5759396244696f7af647a7f
Adam Marsala <adammarsala@hotmail.com>
2021-02-15 22:36:05 +0000

    Corrected templateVars longURL key value

--- express_server.js
+++ express_server.js
@@ -26,7 +26,7 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/:shortURL', (req, res) => {
   console.log(req.params);
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
