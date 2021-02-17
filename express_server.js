60fe91fa306b274469dab2e171d7bf3b8af37f50
Adam Marsala <adammarsala@hotmail.com>
2021-02-17 01:00:03 +0000

    Implemented login and logout using a cookie

--- express_server.js
+++ express_server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Set view engine to ejs 
 app.set('view engine', 'ejs');
 
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,24 +31,32 @@ function generateRandomString(length) {
 app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
   let newShortURL = generateRandomString(6);
-  urlDatabase[newShortURL] = `http://${req.body.longURL}`;
-  console.log(urlDatabase);
+  urlDatabase[newShortURL] = `${req.body.longURL}`;
   res.redirect(`/urls/${newShortURL}`);   // Respond with redirect (302) to new short URL -302 means URI of requested resource has changed temporarily
 });
 
+app.post("/login", (req, res) => {
+  // set cookie - key=username & value=req.body.username (the input form from _header.ejs)
+  res.cookie("username", req.body.username);
+  res.redirect("/urls/");
+});
+
+app.post("/logout", (req, res) => {
+  // clear username cookie
+  res.clearCookie("username");
+  res.redirect("/urls/");
+});
+
 app.post('/urls/:shortURL/edit', (req, res) => {
-  const newLongURL = `http://${req.body.longURLEdit}`;
+  const newLongURL = `${req.body.longURLEdit}`;
   // console.log(req.params.shortURL);
   // console.log("New longURL: " + newLongURL);
   if (Object.values(urlDatabase).includes(newLongURL)) {
     console.log("You already have a short URL for this website URL");
-    res.redirect("/urls/");
   } else {
     urlDatabase[req.params.shortURL] = newLongURL;
-    res.redirect("/urls");
   }
-  //console.log(urlDatabase);
-  
+  res.redirect("/urls");
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -67,12 +77,18 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/urls', (req, res) => {
   // 'urls' will be our variable to for urlDatabase in our template file urls_index.ejs
-  const templateVars = { urls: urlDatabase }; 
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
+  res.render("urls_new", templateVars);
 }); // must be above /urls/:id...routes should be ordered from most to least specific...
 
 app.get("/u/:shortURL", (req, res) => {
@@ -82,7 +98,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get('/urls/:shortURL', (req, res) => {
   // save template variables based on url requested (req.params)
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+    };
   
   if (urlDatabase[req.params.shortURL]) {
     res.render("urls_show", templateVars);
@@ -94,10 +114,13 @@ app.get('/urls/:shortURL', (req, res) => {
 
 // Catch any other request not caught by the above
 app.get("*", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.status(404).render("404.ejs", templateVars);
-});  // curl -i http://localhost:8080/hello shows the entire HTTP response string (headers, html content)
-
+});  
 
 
 app.listen(PORT, () => {
